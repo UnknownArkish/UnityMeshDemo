37ac031642d70cd7272465e86670d40716436782
Arkish <645969958@qq.com>
2019-08-06 01:14:25 +0800

    添加了TestMesh和TestSharedMesh

---
+++ Assets/Scripts/TestMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[]
+        {
+            Vector3.zero,
+            new Vector3( 0, 1, 0 ),
+            new Vector3( 1, 0, 0 ),
+            new Vector3( 1, 1, 0 ),
+        };
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2,
+        };
+        mesh.triangles = triangles;
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+        meshFilter.mesh = mesh;
+
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if( meshRenderer == null)
+        {
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestSharedMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSharedMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh cubeSharedMesh = meshFilter.sharedMesh;
+        cubeSharedMesh.Clear();
+        
+        Vector3[] vertices = new Vector3[]
+        {
+            Vector3.zero,
+            new Vector3( 0, 1, 0 ),
+            new Vector3( 1, 0, 0 ),
+            new Vector3( 1, 1, 0 ),
+        };
+        cubeSharedMesh.vertices = vertices;
+
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2,
+        };
+        cubeSharedMesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
