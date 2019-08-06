c2e1f2990976b796b454819f91c3c354120ea207
Arkish <645969958@qq.com>
2019-08-07 00:26:22 +0800

    添加了TestCombineMesh和TestSubMesh的代码、场景

---
+++ Assets/Scripts/TestMeshCombine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMeshCombine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 收集要合并的物体的所有Mesh信息
+        MeshFilter[] childMeshFilters = GetComponentsInChildren<MeshFilter>();
+
+        CombineInstance[] destCombineInstances = new CombineInstance[childMeshFilters.Length];
+        for ( int i = 0; i < childMeshFilters.Length; i++)
+        {
+            destCombineInstances[i] = new CombineInstance();
+            destCombineInstances[i].mesh = childMeshFilters[i].mesh;
+            destCombineInstances[i].transform = childMeshFilters[i].transform.localToWorldMatrix;
+
+            // 隐藏子物体，或者Destory
+            childMeshFilters[i].gameObject.SetActive(false);
+        }
+        Mesh destMesh = new Mesh();
+        // 进行合并
+        destMesh.CombineMeshes(destCombineInstances, true);
+        destMesh.RecalculateNormals();
+
+        // 将合并后的mesh赋给当前的MeshFilter
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null) meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = destMesh;
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if( meshRenderer == null)
+        {
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            // 设置MeshRenderer的material
+            Material material = new Material(Shader.Find("Standard"));
+            meshRenderer.material = material;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestSubMesh.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSubMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            Vector3.zero,
+            new Vector3( 0, 1, 0 ),
+            new Vector3( 1, 0, 0 ),
+            new Vector3( 1, 1, 0 ),
+        };
+        int[] subTriangle_0 = new int[]
+        {
+            0, 1, 2
+        };
+        int[] subTriangle_1 = new int[]
+        {
+            1, 3, 2
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.subMeshCount = 2;
+        mesh.SetTriangles(subTriangle_0, 0);
+        mesh.SetTriangles(subTriangle_1, 1);
+        mesh.RecalculateNormals();
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null) meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer == null) gameObject.AddComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
